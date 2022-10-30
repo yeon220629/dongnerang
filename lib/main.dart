dff30dd1500fab429668006be53a129d4a5c10d0
yeon220629 <yeon220629@naver.com>
2022-10-30 22:39:21 +0900

    main color 설정!

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 // import 'package:dongnerang/firebase_options.dart';
+import 'package:dongnerang/constants/colors.constants.dart';
 import 'package:dongnerang/screens/splash.screen.dart';
 import 'package:dongnerang/services/user.service.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -7,6 +8,7 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'constants/common.constants.dart';
+
 
 
 bool isFirebaseReady = true;
@@ -20,6 +22,27 @@ void main() async {
   runApp(const MyApp());
 }
 
+class ColorService { //기본 컬러 설정
+  static MaterialColor createMaterialColor(Color color) {
+    List strengths = <double>[.05];
+    Map<int, Color> swatch = {};
+    final int r = color.red, g = color.green, b = color.blue;
+
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+    for (var strength in strengths) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    }
+    return MaterialColor(color.value, swatch);
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -29,8 +52,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: '동네랑',
       theme: ThemeData(
-        // primaryColor: Colors.white,
-        // primarySwatch: Colors.indigo,
+        primaryColor: AppColors.primary,
+        primarySwatch: ColorService.createMaterialColor(const Color(0xff5B88E2)),
       ),
       home: const SplashScreen(),
       // home: const mainScreen(),
--- lib/screens/setting/noticepage.screen.dart
+++ lib/screens/setting/noticepage.screen.dart
@@ -1,3 +1,4 @@
+import 'package:dongnerang/constants/colors.constants.dart';
 import 'package:flutter/material.dart';
 
 import '../../widgets/app_appbar_common.widget.dart';
@@ -29,10 +30,10 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     return Column(
       children: const <Widget>[
         ExpansionTile(
-          title: Text('"동네랑"이 드디어 출시되었습니다!'),
-          subtitle: Text('22.10.28'),
+          title: Text('"동네랑"이 드디어 출시되었습니다!', style: TextStyle(fontWeight: FontWeight.w600, fontSize: 17),),
+          subtitle: Text('22.11.5'),
           children: <Widget>[
-            ListTile(title: Text('동네랑은 2명의 개발자와 함께 알리알라리알리아리리모콘트리아')),
+            ListTile(title: Text('앞으로 동네의 공공소식뿐만 아니라, 동네에서 일어나는 많은 일들을 접할 수 있게 하는 동네랑이 되겠습니다.')),
           ],
         ),
       ],
